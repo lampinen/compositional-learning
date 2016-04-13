5bc3e79528cdf9946718d1f90275765456817ed4
Andrew Lampinen <lampinen@stanford.edu>
2016-04-13 10:31:10 -0700

    Reorganizing & update to compositional

--- GOL/basic_composition/gol_result_averaging.lua
+++ GOL/basic_composition/gol_result_averaging.lua
@@ -7,7 +7,7 @@ require "nn"
 
 
 --learning parameters
-num_training_points = 64
+num_training_points = 256
 num_iterations = 50
 num_averaging_trials = 100
 initial_learning_rate = 0.01
@@ -33,14 +33,15 @@ function three_square_pm_T(i) -- Returns the ith tensor from the enumeration of 
 	return (new_T:reshape(3,3))*2-1
 end
 
+compositional_finals_track = torch.Tensor(num_averaging_trials+1):zero()
 
-hierarchical_avg_track = torch.Tensor(num_iterations+1):zero()
-hierarchical_sloppy_avg_track = torch.Tensor(num_iterations+1):zero()
+compositional_avg_track = torch.Tensor(num_iterations+1):zero()
+compositional_sloppy_avg_track = torch.Tensor(num_iterations+1):zero()
 standard_avg_track = torch.Tensor(num_iterations+1):zero()
 standard_ReLU_avg_track = torch.Tensor(num_iterations+1):zero()
 standard_3_avg_track = torch.Tensor(num_iterations+1):zero()
-hierarchical_MSE_avg_track = torch.Tensor(num_iterations+1):zero()
-hierarchical_sloppy_MSE_avg_track = torch.Tensor(num_iterations+1):zero()
+compositional_MSE_avg_track = torch.Tensor(num_iterations+1):zero()
+compositional_sloppy_MSE_avg_track = torch.Tensor(num_iterations+1):zero()
 standard_MSE_avg_track = torch.Tensor(num_iterations+1):zero()
 standard_ReLU_MSE_avg_track = torch.Tensor(num_iterations+1):zero()
 standard_3_MSE_avg_track = torch.Tensor(num_iterations+1):zero()
@@ -54,7 +55,7 @@ for rseed = 1,num_averaging_trials do
 	trainset = {};
 	trainset_number = {};
 	trainset_select = {};
-	hierarchical_trainset = {};
+	compositional_trainset = {};
 	testset = {};
 	testset_number = {};
 
@@ -79,13 +80,13 @@ for rseed = 1,num_averaging_trials do
 		trainset[i] = {input, output}
 		trainset_number[i] = {input, output_number}
 		trainset_select[i] = {input, output_select}
-		hierarchical_trainset[i] = {torch.Tensor({output_number[1],input[5]}),output}
+		compositional_trainset[i] = {torch.Tensor({output_number[1],input[5]}),output}
 
 	end
 	function trainset:size() return table.getn(trainset) end 
 	function trainset_number:size() return table.getn(trainset_number) end 
 	function trainset_select:size() return table.getn(trainset_select) end 
-	function hierarchical_trainset:size() return table.getn(hierarchical_trainset) end 
+	function compositional_trainset:size() return table.getn(compositional_trainset) end 
 
 
 	for i=1,num_test_points do 
@@ -110,7 +111,7 @@ for rseed = 1,num_averaging_trials do
 	--Network training----------------------------------------------------------
 
 	inputs = 9
-	hierarchical_inputs = 2
+	compositional_inputs = 2
 	HUs = 10
 	outputs = 1
 
@@ -138,7 +139,7 @@ for rseed = 1,num_averaging_trials do
 	standard_3_net:add(nn.Linear(HUs, outputs))
 	standard_3_net:add(nn.Tanh())
 
-	--hierarchical net-----------------------------------------------
+	--compositional net-----------------------------------------------
 	--Count net
 	count_net = nn.Sequential();
 	count_net:add(nn.Linear(inputs, outputs))
@@ -149,29 +150,29 @@ for rseed = 1,num_averaging_trials do
 	select_net:add(nn.Linear(inputs, outputs))
 	select_net:add(nn.ReLU())
 
-	--hierarchical_net
+	--compositional_net
 
-	hierarchical_net = nn.Sequential();
-	hierarchical_net:add(nn.Linear(hierarchical_inputs, HUs))
-	hierarchical_net:add(nn.Tanh())
-	hierarchical_net:add(nn.Linear(HUs, outputs))
-	hierarchical_net:add(nn.Tanh())
+	compositional_net = nn.Sequential();
+	compositional_net:add(nn.Linear(compositional_inputs, HUs))
+	compositional_net:add(nn.Tanh())
+	compositional_net:add(nn.Linear(HUs, outputs))
+	compositional_net:add(nn.Tanh())
 
 	--Alternate: instead of training with true values, trains with outputs of the count_net
-	hierarchical_sloppy_net = nn.Sequential();
-	hierarchical_sloppy_net:add(nn.Linear(hierarchical_inputs, HUs))
-	hierarchical_sloppy_net:add(nn.Tanh())
-	hierarchical_sloppy_net:add(nn.Linear(HUs, outputs))
-	hierarchical_sloppy_net:add(nn.Tanh())
+	compositional_sloppy_net = nn.Sequential();
+	compositional_sloppy_net:add(nn.Linear(compositional_inputs, HUs))
+	compositional_sloppy_net:add(nn.Tanh())
+	compositional_sloppy_net:add(nn.Linear(HUs, outputs))
+	compositional_sloppy_net:add(nn.Tanh())
 
 	--Manual training, for plotting
-	hierarchical_track = {}
-	hierarchical_sloppy_track = {}
+	compositional_track = {}
+	compositional_sloppy_track = {}
 	standard_track = {}
 	standard_ReLU_track = {}
 	standard_3_track = {}
-	hierarchical_MSE_track = {}
-	hierarchical_sloppy_MSE_track = {}
+	compositional_MSE_track = {}
+	compositional_sloppy_MSE_track = {}
 	standard_MSE_track = {}
 	standard_ReLU_MSE_track = {}
 	standard_3_MSE_track = {}
@@ -185,9 +186,9 @@ for rseed = 1,num_averaging_trials do
 		s1_thresh_error = s1_thresh_error + torch.abs(threshold(standard_net:forward(testset[i][1]))[1]-testset[i][2][1])/2
 		s2_thresh_error = s2_thresh_error + torch.abs(threshold(standard_ReLU_net:forward(testset[i][1]))[1]-testset[i][2][1])/2
 		s3_thresh_error = s3_thresh_error + torch.abs(threshold(standard_3_net:forward(testset[i][1]))[1]-testset[i][2][1])/2
-		local hierarchical_input = torch.Tensor({count_net:forward(testset[i][1])[1],select_net:forward(testset[i][1])[1]})
-		h_thresh_error = h_thresh_error + torch.abs(threshold(hierarchical_net:forward(hierarchical_input))[1]-testset[i][2][1])/2
-		hs_thresh_error = hs_thresh_error + torch.abs(threshold(hierarchical_sloppy_net:forward(hierarchical_input))[1]-testset[i][2][1])/2
+		local compositional_input = torch.Tensor({count_net:forward(testset[i][1])[1],select_net:forward(testset[i][1])[1]})
+		h_thresh_error = h_thresh_error + torch.abs(threshold(compositional_net:forward(compositional_input))[1]-testset[i][2][1])/2
+		hs_thresh_error = hs_thresh_error + torch.abs(threshold(compositional_sloppy_net:forward(compositional_input))[1]-testset[i][2][1])/2
 	end
 	s1_thresh_error = s1_thresh_error/num_test_points
 	s2_thresh_error = s2_thresh_error/num_test_points
@@ -197,8 +198,8 @@ for rseed = 1,num_averaging_trials do
 	standard_track[1] = s1_thresh_error
 	standard_ReLU_track[1] = s2_thresh_error
 	standard_3_track[1] = s3_thresh_error
-	hierarchical_track[1] = h_thresh_error
-	hierarchical_sloppy_track[1] = hs_thresh_error
+	compositional_track[1] = h_thresh_error
+	compositional_sloppy_track[1] = hs_thresh_error
 
 	s1_MS_error = 0
 	s2_MS_error = 0
@@ -209,9 +210,9 @@ for rseed = 1,num_averaging_trials do
 		s1_MS_error = s1_MS_error + ((standard_net:forward(testset[i][1]))[1]-testset[i][2][1])^2
 		s2_MS_error = s2_MS_error + ((standard_ReLU_net:forward(testset[i][1]))[1]-testset[i][2][1])^2
 		s3_MS_error = s3_MS_error + ((standard_3_net:forward(testset[i][1]))[1]-testset[i][2][1])^2
-		local hierarchical_input =  torch.Tensor({count_net:forward(testset[i][1])[1],select_net:forward(testset[i][1])[1]})
-		h_MS_error = h_MS_error + ((hierarchical_net:forward(hierarchical_input))[1]-testset[i][2][1])^2
-		hs_MS_error = hs_MS_error + ((hierarchical_sloppy_net:forward(hierarchical_input))[1]-testset[i][2][1])^2
+		local compositional_input =  torch.Tensor({count_net:forward(testset[i][1])[1],select_net:forward(testset[i][1])[1]})
+		h_MS_error = h_MS_error + ((compositional_net:forward(compositional_input))[1]-testset[i][2][1])^2
+		hs_MS_error = hs_MS_error + ((compositional_sloppy_net:forward(compositional_input))[1]-testset[i][2][1])^2
 	end
 	s1_MS_error = s1_MS_error/num_test_points
 	s2_MS_error = s2_MS_error/num_test_points
@@ -221,8 +222,8 @@ for rseed = 1,num_averaging_trials do
 	standard_MSE_track[1] = s1_MS_error
 	standard_ReLU_MSE_track[1] = s2_MS_error
 	standard_3_MSE_track[1] = s3_MS_error
-	hierarchical_MSE_track[1] = h_MS_error
-	hierarchical_sloppy_MSE_track[1] = hs_MS_error
+	compositional_MSE_track[1] = h_MS_error
+	compositional_sloppy_MSE_track[1] = hs_MS_error
 
 
 	for iteration = 1,num_iterations do
@@ -256,18 +257,18 @@ for rseed = 1,num_averaging_trials do
 			select_net:zeroGradParameters()		
 			select_net:backward(trainset_select[i][1],criterion:backward(select_net.output,trainset_select[i][2]))
 			select_net:updateParameters(learning_rate)
-			--hierarchical
-			local hierarchical_input = hierarchical_trainset[i][1]
-			criterion:forward(hierarchical_net:forward(hierarchical_input),trainset[i][2])	
-			hierarchical_net:zeroGradParameters()		
-			hierarchical_net:backward(hierarchical_input,criterion:backward(hierarchical_net.output,trainset[i][2]))
-			hierarchical_net:updateParameters(learning_rate)
-			--hierarchical sloppy
-			local hierarchical_input = torch.Tensor({count_net:forward(trainset[i][1])[1],select_net:forward(trainset[i][1])[1]})
-			criterion:forward(hierarchical_sloppy_net:forward(hierarchical_input),trainset[i][2])	
-			hierarchical_sloppy_net:zeroGradParameters()		
-			hierarchical_sloppy_net:backward(hierarchical_input,criterion:backward(hierarchical_sloppy_net.output,trainset[i][2]))
-			hierarchical_sloppy_net:updateParameters(learning_rate)
+			--compositional
+			local compositional_input = compositional_trainset[i][1]
+			criterion:forward(compositional_net:forward(compositional_input),trainset[i][2])	
+			compositional_net:zeroGradParameters()		
+			compositional_net:backward(compositional_input,criterion:backward(compositional_net.output,trainset[i][2]))
+			compositional_net:updateParameters(learning_rate)
+			--compositional sloppy
+			local compositional_input = torch.Tensor({count_net:forward(trainset[i][1])[1],select_net:forward(trainset[i][1])[1]})
+			criterion:forward(compositional_sloppy_net:forward(compositional_input),trainset[i][2])	
+			compositional_sloppy_net:zeroGradParameters()		
+			compositional_sloppy_net:backward(compositional_input,criterion:backward(compositional_sloppy_net.output,trainset[i][2]))
+			compositional_sloppy_net:updateParameters(learning_rate)
 		end
 
 		--Results on this iteration
@@ -280,9 +281,9 @@ for rseed = 1,num_averaging_trials do
 			s1_thresh_error = s1_thresh_error + torch.abs(threshold(standard_net:forward(testset[i][1]))[1]-testset[i][2][1])/2
 			s2_thresh_error = s2_thresh_error + torch.abs(threshold(standard_ReLU_net:forward(testset[i][1]))[1]-testset[i][2][1])/2
 			s3_thresh_error = s3_thresh_error + torch.abs(threshold(standard_3_net:forward(testset[i][1]))[1]-testset[i][2][1])/2
-			local hierarchical_input =  torch.Tensor({count_net:forward(testset[i][1])[1],select_net:forward(testset[i][1])[1]})
-			h_thresh_error = h_thresh_error + torch.abs(threshold(hierarchical_net:forward(hierarchical_input))[1]-testset[i][2][1])/2
-			hs_thresh_error = hs_thresh_error + torch.abs(threshold(hierarchical_sloppy_net:forward(hierarchical_input))[1]-testset[i][2][1])/2
+			local compositional_input =  torch.Tensor({count_net:forward(testset[i][1])[1],select_net:forward(testset[i][1])[1]})
+			h_thresh_error = h_thresh_error + torch.abs(threshold(compositional_net:forward(compositional_input))[1]-testset[i][2][1])/2
+			hs_thresh_error = hs_thresh_error + torch.abs(threshold(compositional_sloppy_net:forward(compositional_input))[1]-testset[i][2][1])/2
 		end
 		s1_thresh_error = s1_thresh_error/num_test_points
 		s2_thresh_error = s2_thresh_error/num_test_points
@@ -292,8 +293,8 @@ for rseed = 1,num_averaging_trials do
 		standard_track[iteration+1] = s1_thresh_error
 		standard_ReLU_track[iteration+1] = s2_thresh_error
 		standard_3_track[iteration+1] = s3_thresh_error
-		hierarchical_track[iteration+1] = h_thresh_error
-		hierarchical_sloppy_track[iteration+1] = hs_thresh_error
+		compositional_track[iteration+1] = h_thresh_error
+		compositional_sloppy_track[iteration+1] = hs_thresh_error
 
 		s1_MS_error = 0
 		s2_MS_error = 0
@@ -304,9 +305,9 @@ for rseed = 1,num_averaging_trials do
 			s1_MS_error = s1_MS_error + ((standard_net:forward(testset[i][1]))[1]-testset[i][2][1])^2
 			s2_MS_error = s2_MS_error + ((standard_ReLU_net:forward(testset[i][1]))[1]-testset[i][2][1])^2
 			s3_MS_error = s3_MS_error + ((standard_3_net:forward(testset[i][1]))[1]-testset[i][2][1])^2
-			local hierarchical_input = torch.Tensor({count_net:forward(testset[i][1])[1],select_net:forward(testset[i][1])[1]})
-			h_MS_error = h_MS_error + ((hierarchical_net:forward(hierarchical_input))[1]-testset[i][2][1])^2
-			hs_MS_error = hs_MS_error + ((hierarchical_sloppy_net:forward(hierarchical_input))[1]-testset[i][2][1])^2
+			local compositional_input = torch.Tensor({count_net:forward(testset[i][1])[1],select_net:forward(testset[i][1])[1]})
+			h_MS_error = h_MS_error + ((compositional_net:forward(compositional_input))[1]-testset[i][2][1])^2
+			hs_MS_error = hs_MS_error + ((compositional_sloppy_net:forward(compositional_input))[1]-testset[i][2][1])^2
 		end
 		s1_MS_error = s1_MS_error/num_test_points
 		s2_MS_error = s2_MS_error/num_test_points
@@ -316,28 +317,30 @@ for rseed = 1,num_averaging_trials do
 		standard_MSE_track[iteration+1] = s1_MS_error
 		standard_ReLU_MSE_track[iteration+1] = s2_MS_error
 		standard_3_MSE_track[iteration+1] = s3_MS_error
-		hierarchical_MSE_track[iteration+1] = h_MS_error
-		hierarchical_sloppy_MSE_track[iteration+1] = hs_MS_error
+		compositional_MSE_track[iteration+1] = h_MS_error
+		compositional_sloppy_MSE_track[iteration+1] = hs_MS_error
 	end
-	hierarchical_avg_track = hierarchical_avg_track+torch.Tensor(hierarchical_track)
-	hierarchical_sloppy_avg_track = hierarchical_sloppy_avg_track + torch.Tensor(hierarchical_sloppy_track)
+	compositional_avg_track = compositional_avg_track+torch.Tensor(compositional_track)
+	compositional_sloppy_avg_track = compositional_sloppy_avg_track + torch.Tensor(compositional_sloppy_track)
 	standard_avg_track = standard_avg_track + torch.Tensor(standard_track)
 	standard_ReLU_avg_track = standard_ReLU_avg_track + torch.Tensor(standard_ReLU_track)
 	standard_3_avg_track = standard_3_avg_track + torch.Tensor(standard_3_track)
-	hierarchical_MSE_avg_track = hierarchical_MSE_avg_track + torch.Tensor(hierarchical_MSE_track)
-	hierarchical_sloppy_MSE_avg_track = hierarchical_sloppy_MSE_avg_track + torch.Tensor(hierarchical_sloppy_MSE_track)
+	compositional_MSE_avg_track = compositional_MSE_avg_track + torch.Tensor(compositional_MSE_track)
+	compositional_sloppy_MSE_avg_track = compositional_sloppy_MSE_avg_track + torch.Tensor(compositional_sloppy_MSE_track)
 	standard_MSE_avg_track = standard_MSE_avg_track + torch.Tensor(standard_MSE_track)
 	standard_ReLU_MSE_avg_track = standard_ReLU_MSE_avg_track + torch.Tensor(standard_ReLU_MSE_track)
 	standard_3_MSE_avg_track = standard_3_MSE_avg_track + torch.Tensor(standard_3_MSE_track)
+	compositional_finals_track[rseed] = compositional_sloppy_track[num_iterations+1]
+	print(compositional_finals_track)
 end
 
-hierarchical_avg_track = hierarchical_avg_track / num_averaging_trials 
-hierarchical_sloppy_avg_track = hierarchical_sloppy_avg_track  / num_averaging_trials
+compositional_avg_track = compositional_avg_track / num_averaging_trials 
+compositional_sloppy_avg_track = compositional_sloppy_avg_track  / num_averaging_trials
 standard_avg_track = standard_avg_track / num_averaging_trials
 standard_ReLU_avg_track = standard_ReLU_avg_track / num_averaging_trials
 standard_3_avg_track = standard_3_avg_track / num_averaging_trials
-hierarchical_MSE_avg_track = hierarchical_MSE_avg_track / num_averaging_trials
-hierarchical_sloppy_MSE_avg_track = hierarchical_sloppy_MSE_avg_track / num_averaging_trials
+compositional_MSE_avg_track = compositional_MSE_avg_track / num_averaging_trials
+compositional_sloppy_MSE_avg_track = compositional_sloppy_MSE_avg_track / num_averaging_trials
 standard_MSE_avg_track = standard_MSE_avg_track / num_averaging_trials
 standard_ReLU_MSE_avg_track = standard_ReLU_MSE_avg_track / num_averaging_trials
 standard_3_MSE_avg_track = standard_3_MSE_avg_track / num_averaging_trials
@@ -350,14 +353,14 @@ iterations = torch.linspace(0,num_iterations,num_iterations+1)
 
 
 gnuplot.pngfigure('error-rate-plot'..output_figure_name)
---gnuplot.raw('set size square 0.9,0.9')
-gnuplot.plot({'standard',iterations,standard_avg_track},{'standard (1st layer ReLU)',iterations,standard_ReLU_avg_track},{'standard (2 hidden layers)',iterations,standard_3_avg_track},{'hierarchical',iterations,hierarchical_sloppy_avg_track})
+--gnuplot.raw('set size square 0.9,0.9') --For mini abstract figures
+gnuplot.plot({'standard (1 HL, Tanh)',iterations,standard_avg_track},{'standard (1 HL, ReLU)',iterations,standard_ReLU_avg_track},{'standard (2 HL, ReLU, Tanh)',iterations,standard_3_avg_track},{'compositional',iterations,compositional_sloppy_avg_track})
 gnuplot.title('Incorrect response rate after thresholding')
 gnuplot.xlabel('Epoch')
-gnuplot.ylabel('Error rate')
+gnuplot.ylabel('Test error rate')
 gnuplot.pngfigure('MSE-plot'..output_figure_name)
 --gnuplot.raw('set size square 0.9,0.9')
-gnuplot.plot({'standard',iterations,standard_MSE_avg_track},{'standard (1st layer ReLU)',iterations,standard_ReLU_MSE_avg_track},{'standard (2 hidden layers)',iterations,standard_3_MSE_avg_track},{'hierarchical (sloppy training)',iterations,hierarchical_sloppy_MSE_avg_track})
+gnuplot.plot({'standard (1 HL, Tanh)',iterations,standard_MSE_avg_track},{'standard (1 HL, ReLU)',iterations,standard_ReLU_MSE_avg_track},{'standard (2 HL, ReLU, Tanh)',iterations,standard_3_MSE_avg_track},{'compositional',iterations,compositional_sloppy_MSE_avg_track})
 gnuplot.title('Response MSE, w/o thresholding')
 gnuplot.xlabel('Epoch')
-gnuplot.ylabel('MSE')
+gnuplot.ylabel('Test MSE')
